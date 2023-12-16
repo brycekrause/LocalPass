936fbc6ad6669ac0a6c9ccae1d216858927cfa21
imjustjayk <brycekrause@icloud.com>
2023-12-16 14:18:41 -0600

    test

---
+++ lpm.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+title Local Password Manager
+echo 
+echo 
+echo 
+echo 
+echo 			[91m██╗     [31m██████╗ [91m███╗   ███╗[0m
+echo 			[91m██║     [31m██╔══██╗[91m████╗ ████║[0m
+echo 			[91m██║     [31m██████╔╝[91m██╔████╔██║[0m
+echo 			[91m██║     [31m██╔═══╝ [91m██║╚██╔╝██║[0m
+echo 			[91m███████╗[31m██║     [91m██║ ╚═╝ ██║[0m
+echo 			[91m╚══════╝[31m╚═╝     [91m╚═╝     ╚═╝[0m
+pause
